a388aef6f6888445262e2729ed71be2d14bcb317
Erick <santoserick9@gmail.com>
2021-10-04 18:02:18 -0300

    add Dinero.js on project

--- 02-shopping-cart/src/lib/Cart.spec.js
+++ 02-shopping-cart/src/lib/Cart.spec.js
@@ -19,7 +19,7 @@ describe("Cart", () => {
 
   describe("getTotal()", () => {
     it("should return 0 when getTotal() is executed in a newly created instance", () => {
-      expect(cart.getTotal()).toEqual(0);
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
 
     it("should multiply quantity and price and receive the total amount", () => {
@@ -28,7 +28,7 @@ describe("Cart", () => {
         quantity: 2,
       });
 
-      expect(cart.getTotal()).toEqual(70776);
+      expect(cart.getTotal().getAmount()).toEqual(70776);
     });
 
     it("should ensure no more than on product exists at a time", () => {
@@ -42,7 +42,7 @@ describe("Cart", () => {
         quantity: 1,
       });
 
-      expect(cart.getTotal()).toEqual(35388);
+      expect(cart.getTotal().getAmount()).toEqual(35388);
     });
 
     it("should update total when a product gets included and then removed", () => {
@@ -58,7 +58,7 @@ describe("Cart", () => {
 
       cart.remove(product);
 
-      expect(cart.getTotal()).toEqual(41872);
+      expect(cart.getTotal().getAmount()).toEqual(41872);
     });
   });
 
@@ -75,6 +75,32 @@ describe("Cart", () => {
       });
 
       expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it("should return an object with the total and the list of items when summary() is called", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
+    });
+
+    it("should reset the cart when checkout() is called", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.checkout();
+
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
   });
 });
